17e491d002ac0a9a0746d6f94c351083c7baadc7
Gerónimo García <gerog13@hotmail.com>
2022-04-26 23:52:20 -0300

    morgan and express implemented

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 
-const PERSONS = [
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan('tiny'));
+
+let PHONEBOOK = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -24,11 +32,58 @@ const PERSONS = [
   },
 ];
 
+// morgan.token('host', (req, res) => {
+//     return req.hostname;
+// })
+
 app.get("/api/persons", (req, res) => {
-  res.json(PERSONS);
+  res.json(PHONEBOOK);
 });
 
+app.get("/info", (req, res) => {
+  const today = new Date();
+  res.send(`<h3>Phonebook has info for ${PHONEBOOK.length} people</h3>
+        <p>${today}</p>
+    `);
+});
 
+app.get("/api/persons/:id", (req, res) => {
+  let personId = Number(req.params.id);
+  let person = PHONEBOOK.find((p) => p.id === personId);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  let personId = Number(req.params.id);
+  PHONEBOOK = PHONEBOOK.filter((p) => p.id === personId);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", morgan(':method :url :body'), (req, res) => {
+  const body = req.body;
+  if (!body.name) return res.status(404).json({ error: "the name is missing" });
+  if (!body.number)
+    return res.status(404).json({ error: "the number is missing" });
+  let personExists =
+    PHONEBOOK.filter((p) => p.name === body.name).length > 0 ? true : false;
+  if (personExists)
+    return res.status(404).json({ error: "name must be unique" });
+  const person = {
+    id: Math.random() * 10000,
+    name: body.name,
+    number: body.number,
+  };
+  PHONEBOOK = PHONEBOOK.concat(person);
+  res.json({
+    status: res.statusCode,
+    message: `${body.name} was added successfully`,
+  });
+});
 
 const PORT = 3001;
 
